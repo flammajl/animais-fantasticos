302afb2c38334594060b208fc2028888962a40a2
João Luiz <jleaopitta@gmail.com>
2020-03-26 18:37:22 -0300

    Accordion refatorado

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,14 +1,24 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
   }
-  if (accordionList.length) {
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => this.toggleAccordion(item));
     });
+  }
+
+  init() {
+    if (this.accordionList.length) {
+      this.addAccordionEvent();
+    }
+    return this;
   }
 }
