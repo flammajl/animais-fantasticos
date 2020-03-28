17a05f10e32884bc034143d4c25271c2a6da4704
João Luiz <jleaopitta@gmail.com>
2020-03-28 00:10:55 -0300

    Anima-numeros refatorado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,11 +1,11 @@
-import initAnimaNumeros from './anima-numeros.js';
+import AnimaNumeros from './anima-numeros.js';
 
 export default function initFetchAnimais() {
   function createAnimal(animal) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
 
-    div.innerHTML = `<h3>${animal.especie}</h3><span class="data-numero">${animal.total}</span>`;
+    div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
     return div;
   }
 
@@ -19,7 +19,8 @@ export default function initFetchAnimais() {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-      initAnimaNumeros();
+      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+      animaNumeros.init();
     } catch (erro) {
       console.log(erro);
     }
