eb749912b91d0491fef7f7af02c6597a75c8278e
João Luiz <jleaopitta@gmail.com>
2020-03-29 01:26:41 -0300

    Fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,7 +1,7 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker').then((response) => response.json()).then((json) => {
-    const btcPreco = document.querySelector('.btc-preco');
-    btcPreco.innerText = (100 / json.BRL.sell).toFixed(4);
+export default function fetchBitcoin(url, target) {
+  fetch(url).then((response) => response.json()).then((bitcoin) => {
+    const btcPreco = document.querySelector(target);
+    btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
   }).catch((erro) => {
     console.log((Error(erro)));
   });
