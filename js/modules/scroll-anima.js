5c3a756bef3745d154fec59bde7dbca75b57b53e
João Luiz <jleaopitta@gmail.com>
2020-03-29 17:55:06 -0300

    refatoração com class do scroll anima

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,26 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - this.windowMetade;
+      if (sectionTop < 0) {
+        section.classList.add('ativo');
+      } else if (section.classList.contains('ativo')) {
+        section.classList.remove('ativo');
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener('scroll', this.animaScroll);
+    return this;
+  }
+}
